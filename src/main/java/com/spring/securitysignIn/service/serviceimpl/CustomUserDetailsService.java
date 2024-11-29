08f344ae142da7b5664049f097e02cc9ea6b73f7
Nebin Vitus Roy <nebinvitusroy9@gmail.com>
2024-11-29 14:07:09 +0530

    Update CustomUserDetailsService.java

--- src/main/java/com/spring/securitysignIn/service/serviceimpl/CustomUserDetailsService.java
+++ src/main/java/com/spring/securitysignIn/service/serviceimpl/CustomUserDetailsService.java
@@ -3,6 +3,7 @@ package com.spring.securitysignIn.service.serviceimpl;
 import com.spring.securitysignIn.model.CustomUserDetails;
 import com.spring.securitysignIn.model.User;
 import com.spring.securitysignIn.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,7 +15,9 @@ import java.util.Collection;
 import java.util.List;
 
 @Service
+@Slf4j
 public class CustomUserDetailsService implements UserDetailsService {
+
     private final UserRepository userRepository;
 
     public CustomUserDetailsService(UserRepository userRepository){
@@ -23,10 +26,12 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+        log.info("Attempting to load user by username: {}",username);
         User user = userRepository.findByUsername(username);
         if(user == null) {
             throw new UsernameNotFoundException("Username or password Not found");
         }
+        log.info("User loaded successfully: {}",user.getUsername());
         return new CustomUserDetails(user.getUsername(),user.getPassword(),authorities());
     }
     public Collection<? extends GrantedAuthority> authorities(){
